d79c12d035f7f047444cc362a4506db2e6be4b38
Lornz18 <mellgodwinbarza22@gmail.com>
2025-02-27 16:05:31 +0800

    banner 40%

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const burgir = document.querySelector(".toggle_menu");
 const hNav = document.querySelector(".header_nav");
 burgir.addEventListener("click", () => {
   burgir.classList.toggle("open");
-  hnav.classList.toggle("open");
+  hNav.classList.toggle("open");
 });
